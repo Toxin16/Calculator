c2bcdfdcc80411ca2bfcf13e9c72bdfbafe03a20
Toxin16 <109052012+Toxin16@users.noreply.github.com>
2022-08-04 12:48:43 +0900

    Add files via upload

--- calc01.js
+++ calc01.js
@@ -1,13 +1,26 @@
-function display(val){
+let x = document.getElementById('result');
+x.addEventListener("keyup", function(e) {
+    if (e.code === "Enter")   
+        solve();
+})
+
+function display(val){ //텍스트상자에 반환
     document.getElementById('result').value += val
     return val
 }
-function solve(){
+
+function solve(){ // '='버튼 누르면 호출
     let x = document.getElementById('result').value
     let y = eval(x);
     document.getElementById('result').value = y
+    if(y>999999999999) {
+        alert("Math Error")
+    }
     return y
 }
-function clearScreen(){
+
+function clearScreen(){ //C버튼:초기화
     document.getElementById('result').value = ''
 }
+
+function 
